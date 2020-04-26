a710d5adad10b9b7483776b03eccb7892ceddd2e
Debora Lara <debora.roldanlara@gmail.com>
2020-04-26 00:23:54 -0500

    Diretivas

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -15,6 +15,7 @@ import{MatSidenavModule} from '@angular/material/sidenav';
 import{MatListModule} from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { RedDirective } from './directivas/red.directive';
 
 
 @NgModule({
@@ -24,7 +25,8 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     FooterComponent,
     NavComponent,
     HomeComponent,
-    ProductCrudComponent
+    ProductCrudComponent,
+    RedDirective
   ],
   imports: [
     BrowserModule,
