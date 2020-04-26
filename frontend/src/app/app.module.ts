758f6a3e42decbfaa00fddace253d9208818d7a6
Debora Lara <debora.roldanlara@gmail.com>
2020-04-26 14:59:28 -0500

    Gerando componente Tabela

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -22,10 +22,14 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 import { RedDirective } from './directivas/red.directive';
 import { ForDirective } from './directivas/for.directive';
-import { ProductReadComponent } from './components/product/product-read/product-read.component';
 
 
 
@@ -40,7 +44,8 @@ import { ProductReadComponent } from './components/product/product-read/product-
     RedDirective,
     ForDirective,
     ProductCreateComponent,
-    ProductReadComponent
+    ProductReadComponent,
+    ProductRead2Component
   ],
   imports: [
     BrowserModule,
@@ -56,6 +61,9 @@ import { ProductReadComponent } from './components/product/product-read/product-
     FormsModule,
     MatFormFieldModule,
     MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
